ce0aa6b1fe63ec4eedea99a345933b786a8a80ad
WizardMH <52130393+tekcellat@users.noreply.github.com>
2020-04-19 17:08:47 +0300

    Add files via upload

---
+++ Second course/4th semester/Assembler/Lab9/PP.asm
@@ -0,0 +1,27 @@
+.386
+.model FLAT ,C
+PUBLIC p1
+.CODE
+p1:
+	push EBP
+	MOV EBP,ESP
+	MOV EAX,[EBP + 8];N
+	MOV EBX,[EBP + 12];NF
+
+	MOV ECX,[EBP + 8]
+	CMP ECX,1
+	JE M1
+	DEC ECX
+	PUSH EBX,NF
+	
+	PUSH ECX
+	CALL p1
+	ADD ESP,8
+	MOV ECX ,[EBP + 8]
+	MUL ECX
+	JMP M2
+	
+	M1:MOV EAX,1
+	M2:POP ESP
+	ret
+end
