31ea6a45be140718bda56ece1b6dc13f244d281c
WizardMH <52130393+tekcellat@users.noreply.github.com>
2020-05-07 14:37:11 +0300

    Add files via upload

---
+++ Third course/5th semester/CG Course Work/Lighto/Assets/h.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class h : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody2D.velocity = transform.right * 3;
+	}
+}
