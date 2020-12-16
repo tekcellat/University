db6cb9eb651e73742dc94126fd867e42fbf9ed6c
WizardMH <52130393+tekcellat@users.noreply.github.com>
2020-12-16 13:48:09 +0300

    Add files via upload

---
+++ XFouth course/7th semester/Information_Security/Lab2 - Enigma/clean.sh
@@ -0,0 +1,9 @@
+rm archive.decoded
+rm archive.encoded
+rm flag.decoded
+rm flag.encoded
+rm hw.decoded
+rm hw.encoded
+rm Enigma.decoded
+rm Enigma.encoded
+rm -r __pycache__
---
+++ XFouth course/7th semester/Information_Security/Lab2 - Enigma/run.sh
@@ -0,0 +1,8 @@
+python3 main.py -s QWE -e archive.zip
+python3 main.py -s QWE -d archive.encoded
+python3 main.py -s RTY -e flag.txt
+python3 main.py -s RTY -d flag.encoded
+python3 main.py -s ZXC -e hw.py
+python3 main.py -s ZXC -d hw.encoded
+python3 main.py -s UIO -e Enigma.jpg
+python3 main.py -s UIO -d Enigma.encoded
