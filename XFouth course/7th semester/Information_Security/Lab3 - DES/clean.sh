cde1b4719c71c84d842a731ddab9013d6ebf1913
WizardMH <52130393+tekcellat@users.noreply.github.com>
2020-12-16 13:51:35 +0300

    Add files via upload

---
+++ XFouth course/7th semester/Information_Security/Lab3 - DES/clean.sh
@@ -0,0 +1,9 @@
+rm enc_archive.zip
+rm dec_archive.zip
+rm dec_flag.txt
+rm enc_flag.txt
+rm dec_hw.py
+rm enc_hw.py
+rm enc_Enigma.jpg
+rm dec_Enigma.jpg
+rm -r __pycache__
---
+++ XFouth course/7th semester/Information_Security/Lab3 - DES/run.sh
@@ -0,0 +1,4 @@
+python3 main.py archive.zip
+python3 main.py flag.txt
+python3 main.py hw.py
+python3 main.py Enigma.jpg
